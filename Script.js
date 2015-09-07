6eba7bd0f1b43d95036eca79720d8c405d43f9df
misterfifi1 <misterfifi1@users.noreply.github.com>
2015-09-07 12:52:22 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,16 @@
+var input = document.createElement('input');
+input.type = 'button';
+input.value = 'Get Preview Number';
+input.onclick = showAlert;
+document.body.insertBefore(input,document.body.firstChild);
+function showAlert()
+{
+  sum = 0;
+  data = $('a.item_stat');
+  $.each(data, function (key, value) {
+    if (!isNaN(parseInt(value.text))) {
+      sum += parseInt(value.text);
+    }
+  });
+  alert('Page previews: ' + sum);
+}
